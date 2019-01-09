1e049349cf7c2fd27d6ec0717376bee7b42925dd
Khanze99 <khamidov_anvar@mail.ru>
2019-01-09 23:13:25 +0300

    add from parsing in {base.txt}

--- parsing_base/parsing_base.py
+++ parsing_base/parsing_base.py
@@ -6,13 +6,15 @@ input_file_base = open(file_base, mode='r', encoding='utf-8')
 output_file_base = open(file_base_write, mode='w', encoding='utf-8')
 
 
-lookfor = r"[\w.-]+@[A-Za-z-\d]+\.[\w.]+"
+lookfor = r"[\w.-]+@[A-Za-z-\d]+\.[\w.]+"  # (?!intel\.com) убрать из подстроки
+lookfor_names = r""
 
 text = input_file_base.read()
 results = re.findall(lookfor, text)
 
 for item in results:
     print(item)
+    output_file_base.write(item +"\n")
 
 print("Total - ", str(len(results)))
 
