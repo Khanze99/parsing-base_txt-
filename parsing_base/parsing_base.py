9a7de4ce3aaf87a5cfd90a06d0f6a8da76346bd0
Khanze99 <khamidov_anvar@mail.ru>
2019-01-09 22:41:59 +0300

    add total result

--- parsing_base/parsing_base.py
+++ parsing_base/parsing_base.py
@@ -6,11 +6,13 @@ input_file_base = open(file_base, mode='r', encoding='utf-8')
 output_file_base = open(file_base_write, mode='w', encoding='utf-8')
 
 
-lookfor = r"[\w.-]+@[A-Za-z-]+\.[\w.]+"
+lookfor = r"[\w.-]+@[A-Za-z-\d]+\.[\w.]+"
 
 text = input_file_base.read()
 results = re.findall(lookfor, text)
 
 for item in results:
     print(item)
+
+print("Total - ", str(len(results)))
 
