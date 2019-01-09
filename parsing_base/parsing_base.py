53f5e1040d0b01e9b71fefa07d55582c7d0395be
Khanze99 <khamidov_anvar@mail.ru>
2019-01-10 01:26:56 +0300

    add parsing name and city in the text

--- parsing_base/parsing_base.py
+++ parsing_base/parsing_base.py
@@ -6,15 +6,19 @@ input_file_base = open(file_base, mode='r', encoding='utf-8')
 output_file_base = open(file_base_write, mode='w', encoding='utf-8')
 
 
-lookfor = r"[\w.-]+@[A-Za-z-\d]+\.[\w.]+"  # (?!intel\.com) убрать из подстроки
-lookfor_names = r""
+look_for = r"[\w.-]+@[A-Za-z-\d]+\.[\w.]+"  # (?!intel\.com) убрать из подстроки
+look_for_names_and_city = r"(?![A-Z][a-z]+@)[A-Z][\w-]+@?"
 
 text = input_file_base.read()
-results = re.findall(lookfor, text)
+results = re.findall(look_for, text)
+results_names_city = re.findall(look_for_names_and_city, text)
 
 for item in results:
     print(item)
-    output_file_base.write(item +"\n")
+    output_file_base.write(item + "\n")
+
+for item in results_names_city:
+    print(item)
 
 print("Total - ", str(len(results)))
 
