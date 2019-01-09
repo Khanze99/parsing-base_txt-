6b905e64b2bd14a426467d45d98180d665a8d61a
Khanze99 <khamidov_anvar@mail.ru>
2019-01-09 21:25:57 +0300

    First commit for parsing_base

---
+++ parsing_base/parsing_base.py
@@ -0,0 +1,11 @@
+import re
+
+file_base = "base.txt"
+file_base_write = "result_parsing.txt"
+input_file_base = open(file_base, mode='r', encoding='utf-8')
+output_file_base = open(file_base_write, mode='w', encoding='utf-8')
+
+
+lookfor = r""
+
+
